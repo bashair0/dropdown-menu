c3b339633af23ad390658d9fb111ff36463da866
bashair0 <bisho5.ba@gmail.com>
2024-08-01 17:07:12 +0800

    Add dropdown menu and make it functional

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './style.css'
+
+export const dropdown = function () {
+  console.log('This is a message from the demo')
+  document.addEventListener('click', event => {
+    const isDropdownButton = event.target.matches('[data-dropdown-button]')
+    if (!isDropdownButton && event.target.closest('[data-dropdown]') != null)
+      return
+
+    let currentDropdown
+    if (isDropdownButton) {
+      currentDropdown = event.target.closest('[data-dropdown]')
+      currentDropdown.classList.toggle('active')
+    }
+
+    document.querySelectorAll('[data-dropdown].active').forEach(dropdown => {
+      if (dropdown === currentDropdown) return
+      dropdown.classList.remove('active')
+    })
+  })
+}
